dc17091768d64601890c97ce34a7801ba0112980
Emad Hassan <emad95661@gmail.com>
2023-01-31 17:24:01 +0300

    add middleware

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
--- middleware/midlleware.go
+++ middleware/midlleware.go
@@ -2,17 +2,34 @@ package middleware
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"time"
+    "github.com/joho/godotenv"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 )
 
 
 func CheckToken(c *fiber.Ctx) error {
+	err := godotenv.Load()
+if err != nil {
+	return err
+}
+
+secretKey := os.Getenv("SECRET_KEY")
 
 	// Get the token from the request header
 	tokenString := c.Get("Authorization")
+	if len(tokenString) < 7 || strings.ToLower(tokenString[0:7]) != "bearer " {
+		c.Status(501).JSON(fiber.Map{
+			"messege": "Authorization header format must be 'Bearer [token]'",
+		})
+		
+	}
 
+	// Remove the "bearer" keyword from the token string
+	 tokenString = tokenString[7:]
 	// Parse the token
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// Verify that the token was signed with the correct secret key
@@ -21,7 +38,7 @@ func CheckToken(c *fiber.Ctx) error {
 		}
 
 		// Return the secret key used for signing
-		return []byte("secret_key"), nil
+		return []byte(secretKey), nil
 	})
 
 	// Check if the token is valid
